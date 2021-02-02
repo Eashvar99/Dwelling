dd471f09902540dc6a62db906250891781c91625
Eashvar Balachandran <eashvar.balachandran@uoit.net>
2021-02-01 19:28:44 -0500

    Setting Up Async Loading

---
+++ Dwelling/Assets/_Scripts/GameFlow.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameFlow : MonoBehaviour
+{
+   
+    public float loadRange;
+
+    //Scene State
+    private bool isLoaded;
+    private bool shouldLoad;
+
+    void Start()
+    {
+        if(SceneManager.sceneCount > 0)
+        {
+            for(int i = 0; i < SceneManager.sceneCount; ++i)
+            {
+                Scene scene = SceneManager.GetSceneAt(i);
+                if(scene.name == gameObject.name)
+                {
+                    isLoaded = true;
+                }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TriggerCheck();
+    }
+
+    void TriggerCheck()
+    {
+        if(shouldLoad)
+        {
+            LoadScene();
+        }
+        else
+        {
+            UnLoadScene();
+        }
+    }
+
+    void LoadScene()
+    {
+        if(!isLoaded)
+        {
+            SceneManager.LoadSceneAsync(gameObject.name, LoadSceneMode.Additive);
+            isLoaded = true;
+        }
+    }
+
+    void UnLoadScene()
+    {
+        if(isLoaded)
+        {
+            SceneManager.UnloadSceneAsync(gameObject.name);
+            isLoaded = false;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other) 
+    {
+        if(other.CompareTag("Player"))
+        {
+            shouldLoad = true;
+        }
+    }
+    private void OnTriggerExit(Collider other) 
+    {
+        if(other.CompareTag("Player"))
+        {
+            shouldLoad = false;
+        }
+    }
+}
---
+++ Dwelling/Assets/_Scripts/MainMenu.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    
+    List<AsyncOperation> scenesToLoad = new List<AsyncOperation>();
+    public void Load()
+    {
+        scenesToLoad.Add(SceneManager.LoadSceneAsync("Gameplay"));
+        scenesToLoad.Add(SceneManager.LoadSceneAsync("Scene_01", LoadSceneMode.Additive));
+        StartCoroutine(LoadingScreen());  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator LoadingScreen()
+    {
+        float totalProgress = 0;
+        for (int i =0; i < scenesToLoad.Count;  ++i)
+        {
+            while(!scenesToLoad[i].isDone)
+            {
+                totalProgress += scenesToLoad[i].progress;
+                Debug.Log(totalProgress/scenesToLoad.Count);
+                yield return null;
+            }
+        }
+    }
+}

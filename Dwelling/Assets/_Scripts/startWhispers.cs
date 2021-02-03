5d00679463c21e66d9187864f325521dbfcdaa6f
Eashvar Balachandran <eashvar.balachandran@uoit.net>
2021-02-03 13:17:35 -0500

    Whispering in Scene2

---
+++ Dwelling/Assets/_Scripts/startWhispers.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class startWhispers : MonoBehaviour
+{
+    public AudioSource audio1;
+    public AudioSource audio2;
+
+    private void OnTriggerEnter(Collider other) 
+    {
+        if(other.CompareTag("Player"))
+        {
+            
+            audio1.Play();
+            audio2.Play();
+        }
+    }
+}
---
+++ Dwelling/Assets/_Scripts/stopWhispers.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class stopWhispers : MonoBehaviour
+{
+    public AudioSource audio1;
+    public AudioSource audio2;
+    
+    static float t = 0.0f;
+
+    bool doLerp = false;
+
+    private void Update() 
+    {
+        if(t<=1 && doLerp == true)
+        {
+            audio1.volume = Mathf.Lerp(audio1.volume, 0, t);
+            audio2.volume = Mathf.Lerp(audio2.volume, 0, t);
+            t += 0.05f * Time.deltaTime;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other) 
+    {
+        if(other.CompareTag("Player"))
+        {
+            //audio1.Stop();
+            //audio2.Stop();
+            doLerp = true;
+        }
+    }
+}
